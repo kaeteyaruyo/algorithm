23ebacaf88d7504e197f899d7aeac47ca99a62f7
kaeteyaruyo <kaeteyaruyo@gmail.com>
2018-03-26 14:14:16 +0800

    homework 1, problem 4

---
+++ hw1_4.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#define N 5
+
+int merge_count = 0;
+int bubble_count = 0;
+
+void merge_sort(int arr[], int reg[], int l, int r);
+void bubble_sort(int arr[], int n);
+void print(int arr[], int n);
+
+int main(){
+
+    int arr[N] = {9,1,0,5,4};
+    int reg[N] = {0};
+    print(arr,N);
+    merge_sort(arr,reg,0,N-1);
+    print(arr,N);
+
+    int arr1[N] = {9,1,0,5,4};
+    bubble_sort(arr1,N);
+    print(arr1,N);
+
+    printf("merge: %d\nbubble: %d\n",merge_count, bubble_count);
+    return 0;
+
+}
+
+void merge_sort(int arr[], int reg[], int l, int r){
+
+    if(l>=r) return;
+
+    int m = (l+r)/2;
+    merge_sort(arr,reg,l,m);
+    merge_sort(arr,reg,m+1,r);
+
+    int i = l, j = m+1, k = l;
+    while(i <= m && j <= r){
+        if(arr[i] < arr[j]){
+            reg[k++] = arr[i++];
+        }
+        else{
+            reg[k++] = arr[j++];
+            merge_count += m - i + 1;
+        }
+    }
+
+    while(i <= m) reg[k++] = arr[i++];
+    while(j <= r) reg[k++] = arr[j++];
+    for(k = l; k <= r; ++k) arr[k] = reg[k];
+
+}
+
+void bubble_sort(int arr[], int n){
+
+    int i,j;
+    for(i = 0; i < n - 1; ++i){
+        for(j = 0; j < n - 1 - i; ++j){
+            if(arr[j] > arr[j+1]){
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                bubble_count++;
+            }
+        }
+    }
+
+}
+
+void print(int arr[], int n){
+
+    int i;
+    for(i = 0; i < n; ++i) printf("%d ", arr[i]);
+    printf("\n");
+
+}
